147c62ea8e14daa8dc12be633b2fd5182f61891b
Fedorov Vladislav <NextFenixFVE@gmail.com>
2023-02-16 09:22:35 +0700

    Add project files.

---
+++ Grid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMF1;
+
+public struct Point
+{
+    public double x;
+    public double y;
+
+    public Point(double x, double y)
+    {
+        this.x = x; this.y = y;
+    }
+}
+public class Grid
+{
+
+}
---
+++ Managers/FileManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMF1.Managers;
+
+public class FileManager
+{
+    public static Grid ReadGrid(string gridFileName)
+    {
+        using (var streamReader = new FileStream(gridFileName, FileMode.Open, FileAccess.Read))
+        using (var reader = new StreamReader(streamReader, Encoding.UTF8))
+        {
+
+        }
+
+
+        return null;
+    }
+}
